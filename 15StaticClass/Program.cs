65bd07fd93be2f5f895c99ef19fa20ec2bf22c98
PuPandora <pandora6722@gmail.com>
2023-07-27 00:41:13 +0900

    Static Class 정적 클래스 정리

--- 15StaticClass/Program.cs
+++ 15StaticClass/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 정적 클래스란
+// 정적 멤버변수와
+// 정적 멤버함수만을 가질 수 있는 클래스이다.
+static class StClass
+{
+    // 일반 멤버변수는 선언만해도 오류가 발생한다.
+    // int TEST = 10;
+}
+
 namespace _15StaticClass
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CONSOLE WRITELINE은...");
         }
     }
 }
