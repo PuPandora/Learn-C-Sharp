7c8e47773d229676dfff7937cbfd628268c2b4e4
PuPandora <pandora6722@gmail.com>
2023-07-27 01:58:25 +0900

    Switch 정리

--- 20Switch/Program.cs
+++ 20Switch/Program.cs
@@ -8,8 +8,45 @@ namespace _20Switch
 {
     internal class Program
     {
+        // Test Func
+        static int Number100()
+        {
+            return 100;
+        }
+            
         static void Main(string[] args)
         {
+            int Left = 100;
+            int Right = 10;
+
+            if (Left == Right)
+            {
+
+            }
+
+            // Test Code
+            if (Number100() == 100) {
+                Console.WriteLine("함수 리턴 100, 100 비교 결과 true");
+            }
+
+
+            //int Test1 = 50;
+            int SwitchTest = 1;
+
+            switch (SwitchTest)
+            {
+                //case Test1: // 변수는 안 된다. 상수와만 비교할 수 있다.
+                    //break;
+                case 0:
+                case 1:
+                    Console.WriteLine("SwitchTest는 0 또는 1 입니다..");
+                    break;
+                case 100:
+                    Console.WriteLine("SwitchTest는 100 입니다.");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
