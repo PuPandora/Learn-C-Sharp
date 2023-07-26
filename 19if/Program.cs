a85f23f52a380dbe414c1a1c51bf5fb6bd4f8ba1
PuPandora <pandora6722@gmail.com>
2023-07-27 01:46:43 +0900

    if 정리

--- 19if/Program.cs
+++ 19if/Program.cs
@@ -10,6 +10,25 @@ namespace _19if
     {
         static void Main(string[] args)
         {
+            int ElseIfTest = 50;
+            // 조건문
+            if (ElseIfTest == 10)
+            {
+                Console.WriteLine("Test는 10입니다.");
+            }
+
+            else if (ElseIfTest == 100)
+            {
+                // 위의 조건과 연계되어
+                // 위의 조건이 "아니라면"(false) 이 조건으로 넘어온다.
+                Console.WriteLine("Test는 100입니다.");
+            }
+
+            else
+            {
+                // 위의 조건이 모두 거짓이라면 실행한다.
+                Console.WriteLine("모든 조건은 거짓입니다.");
+            }
         }
     }
 }
