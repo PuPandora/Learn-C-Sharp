12be9eb1dc8f36ce276ba85388ad3f235bcd47b2
PuPandora <103708353+PuPandora@users.noreply.github.com>
2023-07-31 11:35:59 +0900

    상속 연습
    + 한 쪽이 죽을 때 까지 배틀 시스템 구현

---
+++ 01Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player : Unit
+{
+    // 상속되는 클래스에서 선언 및 초기화 정보를 받고
+    // 이 클래스의 멤버 변수와 함수(+생성자)를 실행한다 
+
+    int Karma = 0;
+    int Gold = 0;
+
+    // 상속 부모의 생성자를 호출할려면
+    // 상속 문법과 동일하게 생성자 뒤에
+    // : base(매개변수)를 사용한다.
+    public Player(string name, int dmg, int hp, int maxHp, int level) : base(name, dmg, hp, maxHp, level)
+    {
+
+    }
+}
+
+class Monster : Unit
+{
+    int Exp;
+    int DropGold;
+
+    public Monster(string name, int dmg, int hp, int maxHp, int level) : base(name, dmg, hp, maxHp, level)
+    {
+        this.Name = name;
+    }
+}
+
+namespace _01Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer = new Player("용사", 10, 100, 100, 5);
+            Monster NewMonster = new Monster("오크", 5, 50, 50, 3);
+
+            NewPlayer.PrintStatus();
+            NewMonster.PrintStatus();
+
+            NewPlayer.BattleUntilDead(NewMonster);
+
+        }
+    }
+}
---
+++ 01Inheritance/Unit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Unit
+{
+    protected string Name = "None";
+    protected int Level = 0;
+    protected int Dmg = 0;
+    protected int HP = 0;
+    protected int MaxHP = 0;
+
+    // protected 함수 Main에서 접근 불가
+    public void PrintStatus()
+    {
+        Console.WriteLine($"{Name}의 정보 ---------------------");
+        Console.WriteLine($"LV. {Level}");
+        Console.WriteLine($"DMG : {Dmg} ");
+        Console.WriteLine($"HP : {HP} / {MaxHP}");
+        Console.WriteLine("---------------------------------\n\n");
+    }
+
+    public void Battle(Unit otherUnit)
+    {
+        this.HP -= otherUnit.Dmg;
+        otherUnit.BattleMessage();
+        this.PrintStatus();
+
+        otherUnit.HP -= this.Dmg;
+        BattleMessage();
+        otherUnit.PrintStatus();
+    }
+
+    public void BattleMessage()
+    {
+        Console.WriteLine($"{this.Name}의 공격, {this.Dmg}만큼의 대미지를 입혔다.");
+    }
+
+    public void BattleUntilDead(Unit otherUnit)
+    {
+        while(this.HP > 0 && otherUnit.HP > 0)
+        {
+            Battle(otherUnit);
+        }
+    }
+
+    public Unit(string name, int dmg, int hp, int maxHp, int level)
+    {
+        this.Name = name;
+        this.Dmg = dmg;
+        this.HP = hp;
+        this.MaxHP = maxHp;
+        this.Level = level;
+    }
+}
+
+internal class UnitBuilder
+{
+
+}
