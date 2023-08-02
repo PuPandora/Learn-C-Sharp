14f07aa9dab1d64cd7585b767493fc7a9e6a578f
Pandora <pandora6722@gmail.com>
2023-08-02 17:53:48 +0900

    인터페이스 파일 생성

---
+++ 10Interface/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 인터페이스
+// 클래스와 비슷한 구조이지만
+// 멤버가 추상 메소드, 추상 속성, 추상 인덱서, 이벤트로 한정됨.
+
+namespace _10Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
