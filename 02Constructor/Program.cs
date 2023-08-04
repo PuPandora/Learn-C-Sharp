640e535a9cf2dd562a1a7cc06f0a9acd3197d890
Pandora <pandora6722@gmail.com>
2023-08-04 09:01:27 +0900

    클래스 생성자 설명용

---
+++ 02Constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+    string name;
+
+    public Player(string _name)
+    {
+        name = _name;
+    }
+}
+
+namespace _02Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
