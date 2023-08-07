a86f53d1719baface51add27f0ccf4cb47558d14
Pandora <pandora6722@gmail.com>
2023-08-08 08:45:41 +0900

    프로퍼티 정리
    + 2023-08-04 커밋 깜빡한 것

---
+++ 26Property/Property.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+    int AT = 10;
+
+    int GetAT()
+    {
+        return AT;
+    }
+
+    void SetAT(int _value)
+    {
+        AT = _value;
+    }
+}
+
+namespace _26Property
+{
+    internal class Property
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
