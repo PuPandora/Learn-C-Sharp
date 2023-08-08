a427ef91d1caefa66318f766b27b151972fad74e
Pandora <pandora6722@gmail.com>
2023-08-09 08:44:20 +0900

    클래스 파일 분리 공부

--- 27ClassFile/Program.cs
+++ 27ClassFile/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _27ClassFile
+// Ctrl + K + F / 자동 코드 정렬
+// 네임스페이스 없어도 된다.
+class Program
 {
-    internal class Program
-    {   
-        static void Main(string[] args)
-        {
-        }
+    static void Main(string[] args)
+    {
+        // 다른 파일에 있는 클래스를 불러올 수 있다.
+        Player NewPlayer = new Player();
+        Town NewTown = new Town();
+        // 정상적으로 메소드 호출이 가능하다.
+        NewTown.InTown(NewPlayer);
     }
 }
