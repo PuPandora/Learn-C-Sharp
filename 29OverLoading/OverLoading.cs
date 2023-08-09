c2436b54c80fe3477cb93b90d19a6c549f703a22
Pandora <pandora6722@gmail.com>
2023-08-09 09:58:57 +0900

    함수 오버로딩 공부 정리

---
+++ 29OverLoading/OverLoading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// [C# 강의 28화]오버로딩 [어소트락 게임아카데미 게임학원]
+// https://www.youtube.com/watch?v=_N7mccAyXag&t=58s&pp=ygUcQyMg7Ja07IaM7Yq46529IOyYpOuyhOuhnOuUqQ%3D%3D
+
+class OverLoading
+{
+    static void Main(string[] args)
+    {
+        Player NewPlayer = new Player();
+        NewPlayer.Damage(50);
+        NewPlayer.Damage(50, DMGTYPE.FIREDMG);
+    }
+}
---
+++ 29OverLoading/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum DMGTYPE
+{
+    PYDMG,
+    FIREDMG,
+    ICEDMG,
+}
+
+class Player
+{
+    // 물리 방어
+    int AttDef;
+    // 불 방어
+    int FireDef;
+    // 얼음 방어
+    int IceDef;
+
+    int HP = 100;
+
+    // 함수 오버로딩
+    // 이름이 같아도 받는 매개변수 값이 다르면 다른 함수로 취급된다.
+    // Damageint
+    public void Damage(int _Damage)
+    {
+        HP -= _Damage;
+    }
+    // Damagefloatint
+    public void Damage(float _Damage, int _Type)
+    {
+
+    }
+    // Damageintint
+    public void Damage(int _Damage, DMGTYPE _Type)
+    {
+        // switch 문을 int 같은 자료형으로 받으면 정해진 수 보다 크거나 작은 값을 받을 가능성이 있다.
+        // enum을 쓰면 명시적으로 표현할 수 있어 좋다.
+        switch (_Type)
+        {
+            case DMGTYPE.PYDMG:
+                _Damage -= AttDef;
+                break;
+            case DMGTYPE.FIREDMG:
+                _Damage -= FireDef;
+                break;
+            case DMGTYPE.ICEDMG:
+                _Damage -= IceDef;
+                break;
+            default:
+                break;
+        }
+        Damage(_Damage);
+    }
+
+    public void Func(int _Value, int _Value2)
+    {
+
+    }
+
+    public void Func(int _Value)
+    {
+
+    }
+}
+
