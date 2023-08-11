f4a2f24d2c67c3d9812fe65f8e6f17b9ac7b1b88
Pandora <pandora6722@gmail.com>
2023-08-11 17:54:20 +0900

    인벤토리 공부 1

---
+++ InventorySystem/Inven.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 나는 아이템을 담아두는 인벤토리를 만들고 싶다.
+// 인벤토리를 만들어 질 때
+// 인벤토리가 최대 몇개를 담을 수 있는지부터 정해야한다.
+// + 정하지 않으면 인벤토리를 만들 수도 없어야 한다.
+
+// 이 때 숫자가 2개 필요한데, x와 y이다.
+// 그럼 담을 수 있는 아이템의 총 개수는??? x * y
+
+class Inven
+{
+    Item[] ArrItem;
+}
---
+++ InventorySystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// [C# 강의 32화]인벤토리 만들기 1부+ null 레퍼런스 설명 [어소트락 게임아카데미 게임학원]
+// https://www.youtube.com/watch?v=IuibSYe6qV0&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=32&pp=iAQB
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
