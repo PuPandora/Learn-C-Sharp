e7545659b8db393ad9012213fe5c158b5142be11
Pandora <pandora6722@gmail.com>
2023-08-18 17:32:58 +0900

    프로퍼티 예시 추가

--- 26Property/Property.cs
+++ 26Property/Property.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 class Player
 {
-    int AT = 10;
+    private int AT = 10;
+
+    public int ATProperty
+    {
+        get
+        {
+            if (AT > 20)
+            {
+                Console.WriteLine("20 이상입니다.");
+                return AT;
+            }
+            else
+            {
+                Console.WriteLine("19 이하는 get 할 수 없습니다.");
+                return 0;
+            }
+                
+        }
+        set
+        {
+            if (value > 30)
+            {
+                Console.WriteLine("31 이상은 set 할 수 없습니다.");
+                return;
+            }
+
+            else if (value < 30)
+                AT = value;
+        }
+    }
+    public int readOnly { get; }
 
     int GetAT()
     {
@@ -25,6 +55,9 @@ namespace _26Property
     {
         static void Main(string[] args)
         {
+            Player NewPlayer = new Player();
+            Console.WriteLine(NewPlayer.ATProperty); // << Get
+            NewPlayer.ATProperty = 32; // << Set
         }
     }
 }
