0a4b2d47edc613d14954a61831fa9aa3dc79ff56
Pandora <pandora6722@gmail.com>
2023-09-05 13:03:43 +0900

    노드형 자료구조 (LinkedList) 기본 개념 잡기 1

---
+++ LinkedList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Node<T>
+{
+    public Node<T> Next;
+    public Node<T> Prev;
+}
+
+class MyLinkList<T>
+{
+    // LinkedList는 First와 Last를 기억하고 있어 바로 전달해줄 수 있다.
+    public Node<T> First;
+    public Node<T> Last;
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // 시퀀스 배열형 = 그냥 List
+        // 시퀀스 노드형 = LinkedList
+
+        LinkedList<int> LList = new LinkedList<int>();
+
+        // 처음엔 First, Last 의미가 없다.
+        LList.AddFirst(10);
+        //LList.AddLast(10);
+
+        LList.AddFirst(20);
+        LList.AddFirst(33);
+
+        // 33 -> 20 -> 10
+
+        Console.WriteLine("Forward");
+        for (LinkedListNode<int> StartNode = LList.First
+            ; StartNode != null
+            ; StartNode = StartNode.Next)
+        {
+            Console.WriteLine(StartNode.Value);
+        }
+
+        Console.WriteLine("Reverse");
+        for (LinkedListNode<int> StartNode = LList.Last
+            ; StartNode != null
+            ; StartNode = StartNode.Previous)
+        {
+            Console.WriteLine(StartNode.Value);
+        }
+    }
+}
---
+++ Node/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//[C# 강의 41화]LinkedList 학습 및 응용맵이동 시스템 [어소트락 게임아카데미 게임학원]
+// https://www.youtube.com/watch?v=TlWw_-oVXlQ&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=41&pp=iAQB
+
+// 노드는 단순히 자료구조에서만 사용하는 것은 아니다.
+class Node<T>
+{
+    public T Data;
+
+    // 자신안에 자기 자신을 또 가지는 형태로
+    // 되어있는 클래스를 노드라고 할 수 있다.
+    public Node<T> Next = null;
+    public Node<T> Prev = null;
+
+    public Node(T _Data)
+    {
+        Data = _Data;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Node<int> Node1 = new Node<int>(10);
+        Node<int> Node2 = new Node<int>(999);
+        Node<int> Node3 = new Node<int>(578);
+
+        // 노드1 -> 노드2 -> 노드3 참조하는 구조
+        Node1.Next = Node2;
+        Node2.Next = Node3;
+
+        // 노드3 -> 노드2 -> 노드1
+        Node3.Prev = Node2;
+        Node2.Prev = Node1;
+
+        // 현재 노드 변수
+        Node<int> CurNode = Node1;
+        while (CurNode != null)
+        {
+            Console.WriteLine(CurNode.Data);
+            CurNode = CurNode.Next;
+        }
+
+        Console.WriteLine();
+
+        // 반대로 진행되는 노드도 가능하다
+        Node<int> RCurNode = Node3;
+        while (RCurNode != null)
+        {
+            Console.WriteLine(RCurNode.Data);
+            RCurNode = RCurNode.Prev;
+        }
+
+        // 연결되어 있는 노드형 자료구조
+    }
+}
