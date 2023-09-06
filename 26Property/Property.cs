23ed60f53d55c576ed26c35feb20858c16c421a3
Pandora <pandora6722@gmail.com>
2023-09-06 13:06:00 +0900

    프로퍼티 코드 추가

--- 26Property/Property.cs
+++ 26Property/Property.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 class Player
 {
-    private int AT = 10;
-
     public int ATProperty
     {
         get
@@ -36,6 +34,26 @@ class Player
                 AT = value;
         }
     }
+    private int AT = 10;
+    public float HpPercent;
+
+    public int hp
+    {
+        get
+        {
+            return _hp;
+        }
+        set
+        {
+            if (value < 0)
+                value = 0;
+
+            _hp = value;
+        }
+    }
+    private int _hp = 100;
+
+
     public int readOnly { get; }
 
     int GetAT()
@@ -47,6 +65,26 @@ class Player
     {
         AT = _value;
     }
+
+    public int GetHp()
+    {
+        return _hp;
+    }
+
+    public void SetHp(int _value)
+    {
+        _hp = _value;
+    }
+
+    public void AddHP(int _value)
+    {
+        _hp = _hp + _value;
+    }
+}
+
+public class Monster
+{
+    public int AttackDamage = 10;
 }
 
 namespace _26Property
@@ -56,8 +94,33 @@ namespace _26Property
         static void Main(string[] args)
         {
             Player NewPlayer = new Player();
+            /*
             Console.WriteLine(NewPlayer.ATProperty); // << Get
             NewPlayer.ATProperty = 32; // << Set
+            */
+            NewPlayer.hp = 5;
+            int hp = NewPlayer.GetHp();
+            Console.WriteLine(hp);
+            NewPlayer.SetHp(150);
+            Console.WriteLine(NewPlayer.GetHp());
+
+            // get
+            Console.WriteLine(NewPlayer.hp);
+            // set
+            NewPlayer.hp = 170;
+            // get
+            Console.WriteLine(NewPlayer.hp);
+
+            // return 값으로 활용할 때는 get 함수가 실행되고
+            // 값을 대입할 때는 set 함수가 실행된다.
+            // 값을 가져올 때 : get
+            // 값을 수정할 때 : set
+
+            Monster newMonster = new Monster();
+
+            NewPlayer.hp -= newMonster.AttackDamage;
+            NewPlayer.HpPercent = (float)NewPlayer.hp * 0.01f;
+            NewPlayer.hp = (int)NewPlayer.HpPercent;
         }
     }
 }
