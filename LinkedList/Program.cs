0d028478e0b65701856e0bdd4bff9440deb57c1d
Pandora <pandora6722@gmail.com>
2023-09-08 17:02:10 +0900

    노드형 자료구조 기본 이론

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,6 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// [C# 강의 41화]LinkedList 학습 및 응용맵이동 시스템 [어소트락 게임아카데미 게임학원]
+// https://www.youtubehttps://youtu.be/TlWw_-oVXlQcom/@AssortRockGameAcademy
+
+class Zone
+{
+    public string Name = "None";
+    public List<Zone> LinkZone = new List<Zone>();
+
+    public Zone (string _Name)
+    {
+        Name = _Name;
+    }
+}
+
 class Node<T>
 {
     public Node<T> Next;
@@ -24,6 +38,13 @@ internal class Program
         // 시퀀스 배열형 = 그냥 List
         // 시퀀스 노드형 = LinkedList
 
+        // 노드를 관리해주는 자
+        // LinkedList<int>;
+        // LList.First;
+        // LList.Last;
+        // 노드 사이사이를 타고 다니는 클래스
+        // LinkedListNode<int>
+
         LinkedList<int> LList = new LinkedList<int>();
 
         // 처음엔 First, Last 의미가 없다.
@@ -35,7 +56,30 @@ internal class Program
 
         // 33 -> 20 -> 10
 
-        Console.WriteLine("Forward");
+        // 지우는 함수
+        LList.Remove(10);
+        LList.Remove(30);
+        // 첫 번째 노드 삭제
+        LList.RemoveFirst();
+        // 마지막 노드 삭제
+        LList.RemoveLast();
+        // 모든 노드 삭제
+        LList.Clear();
+
+        LList.AddFirst(50);
+        LList.AddFirst(10);
+        LList.AddFirst(90);
+        LList.AddFirst(70);
+
+        // 노드 중간에에 넣는 함수
+        LinkedListNode<int> curNode = LList.First;
+        curNode = curNode.Next;
+        // 현재 노드 Previous에 추가
+        LList.AddBefore(curNode, 8888);
+        // 현재 노드 Next에 추가
+        LList.AddAfter(curNode, 9999);
+
+        Console.WriteLine("\nForward");
         for (LinkedListNode<int> StartNode = LList.First
             ; StartNode != null
             ; StartNode = StartNode.Next)
@@ -43,12 +87,25 @@ internal class Program
             Console.WriteLine(StartNode.Value);
         }
 
-        Console.WriteLine("Reverse");
+        Console.WriteLine("\nReverse");
         for (LinkedListNode<int> StartNode = LList.Last
             ; StartNode != null
             ; StartNode = StartNode.Previous)
         {
             Console.WriteLine(StartNode.Value);
         }
+
+        // - 노드형 자료구조의 특징
+        // 노드형과 배열형의 큰 차이점
+        // 배열형은 연결되어 있는 상태이고 (메모리 크기부터 연결)
+        // 노드는 서로 개별적인 존재이고 이전과 다음 노드를 가르키기만 하기 때문에
+        // 배열형에서 중간 요소에 추가/삭제가 일어날 경우 복잡하고
+        // 배열이 클 수록 처리 규모가 커지지만
+        // 노드형은 단지 가르키는 방향, 즉 가르키는 노드를 바꿔주면
+        // 추가/삭제 작업이 매우 수월하다.
+
+        // 하지만 노드의 특성상 중간에서 시작할 수 없다.
+        // 처음에서 다음, 끝에서 이전 같이 처음과 끝에서만 시작할 수 있다.
+        // (= 순회할 수만 있다)
     }
 }
---
+++ NodeEx/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// [C# 강의 41화]LinkedList 학습 및 응용맵이동 시스템 [어소트락 게임아카데미 게임학원]
+// https://www.youtubehttps://youtu.be/TlWw_-oVXlQcom/@AssortRockGameAcademy
+
+class Zone
+{
+    public string Name = "None";
+    // 갈 수 있는, 이어진 노드의 정보 List
+    public List<Zone> LinkZone = new List<Zone>();
+
+    public Zone(string _Name)
+    {
+        Name = _Name;
+    }
+
+    public Zone Update()
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine("이곳은 " + Name + "입니다.");
+
+            Console.WriteLine("이동할 수 있는 장소 리스트.");
+
+            // 연결된 노드 리스트 출력
+            for (int i = 0; i < LinkZone.Count; i++)
+            {
+                Console.WriteLine((i + 1).ToString() + ". " + LinkZone[i].Name);
+            }
+
+            // enum은 int가 아니다.
+            //ConsoleKey.D1;
+            
+            // 형변환, 형변환이 가능한 것이 있고, 불가능 한 것이 있다.
+            // enum은 int로 변환이 가능하다.
+            int Number = (int)Console.ReadKey().Key;
+            Number -= 49;
+
+            // 간단하게 입력으로 노드 정보를 반환. 잘못 입력하면 인덱스 에러
+            return LinkZone[Number];
+        }
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Zone NewZone0 = new Zone("태초마을");
+        Zone NewZone1 = new Zone("초보사냥터");
+        Zone NewZone2 = new Zone("중급사냥터");
+        Zone NewZone3 = new Zone("중급마을");
+        Zone NewZone4 = new Zone("고급사냥터");
+
+        // 한 노드에서, 다른 곳의 노드로 갈 수 있는 정보를
+        // 각 노드 List에 저장
+
+        // 태초마을 -> 초보사냥터, 중급사냥터
+        NewZone0.LinkZone.Add(NewZone1);
+        NewZone0.LinkZone.Add(NewZone2);
+
+        // 초보사냥터, 중급사냥터 -> 중급마을, -> 초급마을
+        NewZone1.LinkZone.Add(NewZone3);
+        NewZone1.LinkZone.Add(NewZone0);
+        NewZone2.LinkZone.Add(NewZone3);
+        NewZone2.LinkZone.Add(NewZone0);
+
+        // 중급 마을 -> 초급사냥터, 중급사냥터, 고급사냥터
+        NewZone3.LinkZone.Add(NewZone1);
+        NewZone3.LinkZone.Add(NewZone2);
+        NewZone3.LinkZone.Add(NewZone4);
+
+        Zone StartZone = NewZone0;
+
+        while (true)
+        {
+            // 반환된 노드 정보를 현재 노드에 대입
+            // A -> B / A <- B -> C, D...
+            StartZone = StartZone.Update();
+        }
+    }
+}
