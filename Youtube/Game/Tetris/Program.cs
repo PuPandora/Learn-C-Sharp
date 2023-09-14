1ba59ef0117698441c7c064eca75c65f038bc83d
Pandora <pandora6722@gmail.com>
2023-09-14 17:00:10 +0900

    테트리스 만들기 1차

---
+++ Youtube/Game/Tetris/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//C# 강의 43화 테트리스 만들기 1차 [어소트락 게임아카데미 게임학원]
+// https://www.youtube.com/watch?v=Ygv48IyvADI&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=43&pp=iAQB
+
+enum TBLOCK
+{
+    WALL,
+    VOID,
+    BLOCK,
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TScreen newSC = new TScreen(10, 15);
+
+        Block NewBlock = new Block();
+
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                newSC.SetBlock(y, x, TBLOCK.BLOCK);
+            }
+        }
+
+        newSC.Render();
+    }
+}
---
+++ Youtube/Game/Tetris/TScreen.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class TScreen
+{
+    List<List<TBLOCK>> BlockList = new List<List<TBLOCK>>();
+
+    public void SetBlock(int _y, int _x, TBLOCK _Type)
+    {
+        BlockList[_y][_x] = _Type;
+    }
+
+    public void Render()
+    {
+        for (int y = 0; y < BlockList.Count; y++)
+        {
+            for (int x = 0; x < BlockList[y].Count; x++)
+            {
+                switch (BlockList[y][x])
+                {
+                    case TBLOCK.WALL:
+                        Console.Write("▣");
+                        break;
+                    case TBLOCK.VOID:
+                        Console.Write("□");
+                        break;
+                    case TBLOCK.BLOCK:
+                        Console.Write("■");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public TScreen(int _X, int _Y)
+    {
+        // 0, 0 같은걸 넣어주면?
+
+        for (int y = 0; y < _Y; y++)
+        {
+            BlockList.Add(new List<TBLOCK>());
+            for (int x = 0; x < _X; x++)
+            {
+                BlockList[y].Add(TBLOCK.VOID);
+            }
+        }
+
+        for (int i = 0; i < BlockList[0].Count; i++)
+        {
+            BlockList[0][i] = TBLOCK.WALL;
+        }
+
+        for (int i = 0; i < BlockList[BlockList.Count - 1].Count; i++)
+        {
+            BlockList[BlockList.Count - 1][i] = TBLOCK.WALL;
+        }
+    }
+}
