4ac9ff1dc747333c3fea0fdfc4f21a55776f8b2b
Pandora <pandora6722@gmail.com>
2023-09-15 09:51:30 +0900

    테트리스 만들기 2차

--- Youtube/Game/Tetris/Block.cs
+++ Youtube/Game/Tetris/Block.cs
@@ -4,7 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+enum BLOCKTYPE
+{
+    BT_I, // 4칸 블록
+    BT_L, // 갈고리 오른쪽
+    BT_J, // 갈고리 왼쪽
+    BT_Z, // 왼쪽 계단
+    BT_S, // 오른쪽 계단
+    BT_T, // T 블록
+    BT_O, // 네모
+}
+
 internal class Block
 {
+    int X = 0;
+    int Y = 0;
+    List<List<string>> BlockData = new List<List<string>>();
 
+    TETRISSCREEN Screen = null;
+
+    public Block(TETRISSCREEN _Screen)
+    {
+        Screen = _Screen;
+
+        for (int y = 0; y < 4; y++)
+        {
+            BlockData.Add(new List<string>());
+        }
+
+    }
+
+    private void Input()
+    {
+        if (false == Console.KeyAvailable)
+        {
+            return;
+        }
+
+        switch (Console.ReadKey().Key)
+        {
+            case ConsoleKey.A:
+                X -= 1;
+                break;
+            case ConsoleKey.D:
+                X += 1;
+                break;
+            case ConsoleKey.S:
+                Y += 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Move()
+    {
+        Input();
+        Screen.SetBlock(Y, X, "■");
+    }
 }
--- Youtube/Game/Tetris/Program.cs
+++ Youtube/Game/Tetris/Program.cs
@@ -4,32 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//C# 강의 43화 테트리스 만들기 1차 [어소트락 게임아카데미 게임학원]
+// C# 강의 43화 테트리스 만들기 1차 [어소트락 게임아카데미 게임학원]
 // https://www.youtube.com/watch?v=Ygv48IyvADI&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=43&pp=iAQB
-
-enum TBLOCK
-{
-    WALL,
-    VOID,
-    BLOCK,
-}
+// [C# 강의 44화]테트리스 만들기 2차 [어소트락 게임아카데미 게임학원]
+// https://www.youtube.com/watch?v=kXz21sPzES8&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=44
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        TScreen newSC = new TScreen(10, 15);
+        TETRISSCREEN newSC = new TETRISSCREEN(10, 15);
 
-        Block NewBlock = new Block();
+        Block NewBlock = new Block(newSC);
 
-        for (int y = 0; y < 3; y++)
+        while (true)
         {
-            for (int x = 0; x < 3; x++)
+            for (int i = 0; i < 50000000; i++)
             {
-                newSC.SetBlock(y, x, TBLOCK.BLOCK);
+                int a = 0;
             }
+            Console.Clear();
+            newSC.Render();
+            newSC.Clear();
+            NewBlock.Move();
         }
 
-        newSC.Render();
     }
 }
