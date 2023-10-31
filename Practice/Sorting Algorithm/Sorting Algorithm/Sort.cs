afb06bc5289eadfabd4f26547219145746be299b
PuPandora <103708353+PuPandora@users.noreply.github.com>
2023-10-31 15:08:44 +0900

    싱글톤 파일
    다른 레포지토리에 작성했던 싱글톤 파일 옮겨옴
    디자인 패턴도 이 프로젝트에서 다룰 예정

---
+++ Practice/Sorting Algorithm/Sorting Algorithm/Sort.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    public static class Sort
+    {
+        public static void BubbleSort(int[] array)
+        {
+            // 1. (배열의 길이-1) * (배열의 길이-1) 만큼 비교한다.
+            // 2. n번째 요소를 n+1번째 요소와 비교
+            // 2-1. n번째 요소가 크다면 위치 교환
+            // 3. 첫 반복이 끝났다면 가장 큰 숫자가 가장 뒤에 위치 됨
+            // 4. 반복 횟수가 늘어나면, 가장 뒤에 정렬되는 요소 개수가 동일하게 증가한다
+
+            Console.WriteLine("\n===== 버블 정렬 =====\n");
+
+            int comparisonCount = 0;
+            int swapCount = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap
+                        int temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+
+                        swapCount++;
+                    }
+                    comparisonCount++;
+                }
+            }
+            PrintOperationCount(comparisonCount, swapCount);
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            // 1. 배열의 n번째 요소를 minNumberIndex에 저장한다.
+            // 2. 배열의 n+1 인덱스 요소부터 비교를 시작하며,
+            // 배열[minNumberIndex] 요소와 다음 인덱스 요소 값과 비교한다.
+            // 2-1. 만약 다음 요소가 더 작다면, minNumberIndex를 다음 인덱스로 갱신한다.
+            // 3. 전부 순회한 뒤, 배열[n] 요소와 배열[minNumberIndex] 요소를 서로 교환한다.
+            // 4. n+1 하고 2~4 과정을 배열의 길이만큼 반복한다.
+
+            Console.WriteLine("\n===== 선택 정렬 =====\n");
+
+            int comparisonCount = 0;
+            int swapCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minNumberIndex = i;
+                int temp = 0;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    // Comparison
+                    if (array[j] < array[minNumberIndex])
+                    {
+                        minNumberIndex = j;
+                    }
+                    comparisonCount++;
+                }
+
+                // Swap
+                if (i != minNumberIndex)
+                {
+                    temp = array[i];
+                    array[i] = array[minNumberIndex];
+                    array[minNumberIndex] = temp;
+                }
+
+                swapCount++;
+            }
+            PrintOperationCount(comparisonCount, swapCount);
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            // 1. 배열의 길이만큼 반복한다
+            // 2. 현재 인덱스 요소(i)와 현재 진행 중인 인덱스-1 요소(j)를 비교한다.
+            // 3. 만약 i가 작다면 서로 교환한다.
+            // 3-1. i-1 요소와 j-1 요소를 비교하고, i가 작다면 교환한다.
+            // 4. 만약 i가 작지 않다면, 다음 요소 검사로 넘어간다.
+
+            Console.WriteLine("\n===== 삽입 정렬 =====\n");
+
+            int comparisonCount = 0;
+            int swapCount = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int curIndex = i;
+                for (int j = i-1; j >= 0; j--)
+                {
+                    // Comparison
+                    comparisonCount++;
+
+                    if (array[curIndex] < array[j])
+                    {
+                        int temp = array[j];
+                        array[j] = array[curIndex];
+                        array[curIndex] = temp;
+
+                        curIndex--;
+                        swapCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            PrintOperationCount(comparisonCount, swapCount);
+        }
+
+        public static void MergeSort(int[] array)
+        {
+
+        }
+
+        private static void PrintOperationCount(int comparisonCount, int swapCount)
+        {
+            Console.WriteLine($"비교 횟수 : {comparisonCount}\n교환 횟수 : {swapCount}\n");
+            Console.WriteLine("===== ===== ===== =====\n");
+        }
+    }
+}
---
+++ Practice/Sorting Algorithm/Sorting Algorithm/_Program.cs
@@ -0,0 +1,159 @@
+﻿using SortingAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class Array
+{
+    private static Random random = new Random();
+
+    public static void SetArraySize(out int[] array)
+    {
+        Console.Write("생성할 배열 크기를 입력해주세요.\n >> ");
+        int.TryParse(Console.ReadLine(), out int inputSize);
+
+        array = new int[inputSize];
+    }
+
+    public static void SetArraySize(int[] array, int size)
+    {
+        array = new int[size];
+    }
+
+    public static void PrintArrayLength(int[] array)
+    {
+        Console.WriteLine("intArr의 크기 : " + array.Length);
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"Array[{i}] : {array[i]}");
+        }
+    }
+
+    public static void MakeRandomArray(int[] array, int min, int max)
+    {
+        Console.WriteLine("\n===== 랜덤 배열 생성 =====\n");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(min, max);
+        }
+    }
+
+    public static void MakeRandomArray(int[] array, int max)
+    {
+        MakeRandomArray(array, 0, max);
+    }
+
+    public static void MakeSortedArray(int[] array)
+    {
+        Console.WriteLine("\n===== 정렬 배열 생성 =====\n");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i;
+        }
+    }
+}
+
+internal class _Program
+{
+    static int[] intRandArr;
+    static int[] intSortedArr;
+
+    static void Main(string[] args)
+    {
+        // 1. 배열 크기 선택
+        // 2. 정렬 알고리즘 선택 (정렬 전 랜덤으로 섞음)
+        // 3. 프로그램 종료
+        bool programRun = true;
+        while (programRun)
+        {
+            Console.Clear();
+            Console.WriteLine("- 정렬 알고리즘 프로그램 -\n");
+            Console.WriteLine("1. 배열 크기 설정");
+            Console.WriteLine("2. 정렬 알고리즘");
+            Console.WriteLine("3. 프로그램 종료");
+
+            switch(Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Array.SetArraySize(out intRandArr);
+                    Console.Clear();
+                    break;
+
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    SelectSortAlgorithm();
+                    break;
+
+                case ConsoleKey.D3:
+                    Console.Clear();
+                    Console.WriteLine("프로그램을 종료합니다...\n\n");
+                    programRun = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    static void SelectSortAlgorithm()
+    {
+        bool isRun = true;
+        bool isExit = false;
+        bool isDefault = false;
+
+        while (isRun)
+        {
+            Array.MakeRandomArray(intRandArr, 10);
+
+            Console.WriteLine("1. 선택 정렬");
+            Console.WriteLine("2. 버블 정렬");
+            Console.WriteLine("3. 삽입 정렬");
+            Console.WriteLine("4. 나가기");
+
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Array.PrintArray(intRandArr);
+                    Sort.SelectionSort(intRandArr);
+                    break;
+
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    Array.PrintArray(intRandArr);
+                    Sort.BubbleSort(intRandArr);
+                    break;
+
+                case ConsoleKey.D3:
+                    Console.Clear();
+                    Array.PrintArray(intRandArr);
+                    Sort.InsertionSort(intRandArr);
+                    break;
+
+                case ConsoleKey.D4:
+                    Console.Clear();
+                    isRun = false;
+                    isExit = true;
+                    break;
+
+                default:
+                    Console.Clear();
+                    isDefault = true;
+                    break;
+            }
+            if (!isExit && !isDefault)
+                Array.PrintArray(intRandArr);
+        }
+    }
+}
