8fddce4fbeb7497f56442acc3c81971a2da25fda
PuPandora <103708353+PuPandora@users.noreply.github.com>
2024-11-01 11:55:12 +0900

    (Add) char배열, string 객체 대입
    string은 크기가 유동적인 점으로 시작하여, char 배열로 메모리 크기를 한정적으로 사용하는 방식으로 사용해보기 위한 코드

---
+++ Note/CharArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        char[] a = new char[20];
+
+        SetCharArray(ref a, "ABCDEFEFGHIJKLMNOPQRSTUVWXYZ");
+        Console.WriteLine(a);
+    }
+
+    private static void SetCharArray(ref char[] array, string value)
+    {
+        for (int i = 0; i < array.Length && i < value.Length; i++)
+        {
+            array[i] = value[i];
+        }
+    }
+}
